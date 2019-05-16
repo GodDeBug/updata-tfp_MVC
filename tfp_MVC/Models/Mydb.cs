70b2e4b8954d38ee4e33785c8af36b72bf7018da
Magic-Leo <tfp1212@163.com>
2019-05-16 09:36:19 +0800

    my first commit for Asp.net Web

---
+++ tfp_MVC/Models/Mydb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace tfp_MVC.Models
+{
+    public class MyDb : DbContext
+    {
+        public MyDb() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<tfp_MVC.Models.StuCJInfo> Xueshengxinxis { get; set; }
+
+        public System.Data.Entity.DbSet<tfp_MVC.Models.StuInfo> StuInfoes { get; set; }
+    }
+}
