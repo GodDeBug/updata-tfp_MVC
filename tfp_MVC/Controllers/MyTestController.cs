d880be673f7a55979abcb20d161d77e959e80cb6
coderTong <tfp1212@163.com>
2019-05-16 11:08:46 +0800

    My first commit for asp.net web

---
+++ tfp_MVC/Controllers/MyTestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tfp_MVC.Controllers
+{
+    public class MyTestController : Controller
+    {
+        // GET: MyTest
+        public ActionResult Index()
+        {
+            ViewBag.sp = "这是我第一次吃德州扒鸡";
+            return View();
+        }
+    }
+}
